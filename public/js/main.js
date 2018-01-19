cd8332e196b9166f087e51c2f304731cb6006475
User <user@gmail.com>
2018-01-19 11:08:33 -0200

    Signed-off-by: User <user@gmail.com>

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,34 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import Attachments from './attachments.jsx';
+import { render } from 'react-dom';
+import { Alert } from 'react-pipestyle';
 
-ReactDOM.render(<Attachments />, document.getElementById('attachments'));
+const App = () => (
+  <div style={{padding: 15}}>
+    <Alert message="Default alert" />
+    <Alert
+      type="info"
+      message="Info alert"
+      onClickClose={() => { }}
+    />
+    <Alert
+      type="warning"
+      message="Warning alert"
+      onClickClose={() => { }}
+    />
+    <Alert
+      type="danger"
+      icon="alert"
+      message="Danger alert"
+      onClickClose={() => { }}
+    />
+    <Alert
+      big
+      message="Big alert"
+      onClickClose={() => { }}
+    />
+  </div>
+);
+
+render(<App />, document.getElementById('attachments'));
+//ReactDOM.render(<Attachments />, document.getElementById('attachments'));
+
