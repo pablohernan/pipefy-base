8a8619b1f454e37134946dfd5a8b1a61b6591188
User <user@gmail.com>
2018-01-19 11:13:34 -0200

    Signed-off-by: User <user@gmail.com>

--- public/js/main.js
+++ public/js/main.js
@@ -1,34 +1,37 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Alert } from 'react-pipestyle';
+import { Dropdown } from 'react-pipestyle';
 
 const App = () => (
-  <div style={{padding: 15}}>
-    <Alert message="Default alert" />
-    <Alert
-      type="info"
-      message="Info alert"
-      onClickClose={() => { }}
-    />
-    <Alert
-      type="warning"
-      message="Warning alert"
-      onClickClose={() => { }}
-    />
-    <Alert
-      type="danger"
-      icon="alert"
-      message="Danger alert"
-      onClickClose={() => { }}
-    />
-    <Alert
-      big
-      message="Big alert"
-      onClickClose={() => { }}
-    />
+  <div>
+    <Dropdown
+      hasHeader
+      inputLabel="Dropdown medium"
+      inputPlaceholder="Field placeholder"
+      inputOnChange={() => { }}
+      inputTabIndex={1}
+      inputTitle="Field title"
+      hasFooter
+      buttonTitle="Button title"
+      buttonLabel="Click here"
+      buttonTabIndex={1}
+      buttonOnClick={() => { }}
+    >
+      Content
+    </Dropdown>
+    <hr />
+    <Dropdown
+      hasHeader
+      inputLabel="Dropdown large"
+      size="lg"
+    >
+      Content
+    </Dropdown>
+    <hr />
+    <Dropdown>
+      Content
+    </Dropdown>
   </div>
 );
 
 render(<App />, document.getElementById('attachments'));
-//ReactDOM.render(<Attachments />, document.getElementById('attachments'));
-
