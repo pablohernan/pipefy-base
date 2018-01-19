50ae04c04471430e6bbbf3c7e171d0162e3c42dd
User <user@gmail.com>
2018-01-19 11:18:52 -0200

    Signed-off-by: User <user@gmail.com>

--- public/js/main.js
+++ public/js/main.js
@@ -1,39 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Attachments from './attachments.jsx';
-import Dropdown from 'react-pipestyle';
 
-const App = () => (
-  <div>
-    <Dropdown
-      hasHeader
-      inputLabel="Dropdown medium"
-      inputPlaceholder="Field placeholder"
-      inputOnChange={() => { }}
-      inputTabIndex={1}
-      inputTitle="Field title"
-      hasFooter
-      buttonTitle="Button title"
-      buttonLabel="Click here"
-      buttonTabIndex={1}
-      buttonOnClick={() => { }}
-    >
-      Content
-    </Dropdown>
-    <hr />
-    <Dropdown
-      hasHeader
-      inputLabel="Dropdown large"
-      size="lg"
-    >
-      Content
-    </Dropdown>
-    <hr />
-    <Dropdown>
-      Content
-    </Dropdown>
-  </div>
-);
-
-
-ReactDOM.render(<App />, document.getElementById('attachments'));
+ReactDOM.render(<Attachments />, document.getElementById('attachments'));
